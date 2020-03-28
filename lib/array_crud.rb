099e20b7309aeadd9c53fa245d640c08b230f409
Elena Dinca <elenadinca25@gmail.com>
2020-03-28 13:24:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,14 +7,11 @@ my_array = ["", "", "", ""]
 end
 
 def add_element_to_end_of_array(array, element)
-my_array = ["hello", "cat", "dog"]
-my_array.push("hi")
-p my_array
+
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["", "", "", ""]
-  my_array.unshift("ciao")
+
 end
 
 def remove_element_from_end_of_array(array)
