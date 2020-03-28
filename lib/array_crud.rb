7c7368cd7dea65f30745ea575d0201c4daf3a829
Elena Dinca <elenadinca25@gmail.com>
2020-03-28 13:04:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ my_array = ["", "", "", ""]
 end
 
 def add_element_to_end_of_array(array, element)
-array(element).push
+element = "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
